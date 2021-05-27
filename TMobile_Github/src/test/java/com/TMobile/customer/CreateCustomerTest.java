abceec715b40c1f3c3ab3c20736d80563faaf786
hai <hai@Sravanthi>
2021-05-27 14:16:38 +0530

    My First Project Commit

---
+++ TMobile_Github/src/test/java/com/TMobile/customer/CreateCustomerTest.java
@@ -0,0 +1,13 @@
+package com.TMobile.customer;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomerTest {
+
+	@Test
+	public void run()
+	{
+		Reporter.log("Tests Running Successfully",true);
+	}
+}
