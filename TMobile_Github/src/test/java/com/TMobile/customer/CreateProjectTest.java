87871e84e55fe70a542f1fd50ac27d6f258bd152
hai <hai@Sravanthi>
2021-05-27 14:27:55 +0530

    Create Project Commit

---
+++ TMobile_Github/src/test/java/com/TMobile/customer/CreateProjectTest.java
@@ -0,0 +1,14 @@
+package com.TMobile.customer;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateProjectTest {
+	
+	@Test
+	public void gun()
+	{
+		Reporter.log("Create Project Tests Running Successfully",true);
+	}
+
+}
